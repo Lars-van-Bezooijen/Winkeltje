95a80baf751a5968539ef2f8d50abbc008da004a
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-04-05 13:21:22 +0200

    added header

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Winkeltje</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/1dbd977bb4.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <img class="logo" src="img/logo.webp" alt="logo">
+        <nav>
+            <a href="">pagina</a>
+            <a href="">nav</a>
+            <a href="">nog</a>
+        </nav>
+        <i class="fa-solid fa-cart-shopping"></i>
+    </header>
+</body>
+</html>
