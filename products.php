50e49d1f94ed8bdfb24448e425c77e2cf5c219b8
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-04-09 15:32:15 +0200

    added products.php

--- products.php
+++ products.php
@@ -8,14 +8,43 @@
 </head>
 <body>
 <?php 
-require_once "header.php"
-
-
+    require_once "header.php"
 ?>  
+
+<?php
+    require_once 'backend/conn.php';
+    $query = "SELECT * FROM products";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+<div class="wrapper">
+    <?php foreach($products as $product): ?> 
+        <div class="product">
+            <div class="split">
+                <img class="img" src="<?php echo $product['img_dir'] ?>" alt="">              
+                <p class="name"><?php echo $product['name']; ?></p>                
+                <p class="description"><?php echo "Beschrijving: " . $product['description']; ?></p>
+                <p class="price">&euro;<?php echo $product['price']; ?></p>
+                <?php
+                    if($product['stock'] == 0){
+                        echo '<p class="stock-empty">Niet leverbaar!</p>';
+                    }
+                ?>
+            </div>
+            <div class="split">
+                <form class="amount" action="">
+                    <input type="number" name="amount" id="amount" min="0" max="<?php echo $product['stock']; ?>">
+                    <input type="submit" value="in winkelwagen">
+                </form>
+            </div>
+            
+        </div>
+    <?php endforeach; ?>
+</div>
+
 <?php 
-require_once "footer.php"
-
-
+    require_once "footer.php"
 ?>
 </body>
 </html>
